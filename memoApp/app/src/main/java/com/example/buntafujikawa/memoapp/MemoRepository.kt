dfa665bba501d8f921486a443b2f61dc6f678818
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-01-21 23:08:18 +0900

    wip add memo repository

--- memoApp/app/src/main/java/com/example/buntafujikawa/memoapp/MemoRepository.kt
+++ memoApp/app/src/main/java/com/example/buntafujikawa/memoapp/MemoRepository.kt
@@ -2,86 +2,171 @@ package com.example.buntafujikawa.memoapp
 
 import android.content.ContentValues
 import android.content.Context
+import android.content.CursorLoader
+import android.database.Cursor
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
-import java.io.File
-import java.io.FileWriter
-import java.io.IOException
+import java.io.*
 import java.util.*
 
 /**
- * MemoProviderと、Memo情報を必要とするクラスの橋渡しをする
+ * メモ管理用のContentProvider
  */
-class MemoRepository {
+object MemoRepository {
+    // prefix-yyyy-mm-dd-HH-MM-SS.txt
+    private val MEMO_FILE_FORMAT = "%1\$s-%2\$tF-%2\$tH-%2\$tM-%2\$tS.txt"
 
-    companion object {
-        // prefix-yyyy-mm-dd-HH-MM-SS.txt
-        private val MEMO_FILE_FORMAT: String = "%1$s-%2$tF-%2$tH-%2$tM-%2$tS.txt"
+    fun create(context: Context, memo: String): Uri? {
+        var outputDir: File? = getOutputDir(context)
 
-        fun store(context: Context, memo: String): Uri? {
-            var outputDir: File
+        outputDir ?: return null
 
-            if (Build.VERSION.SDK_INT >= 19) {
-                // ドキュメントファイル用共有ディレクトリ
-                outputDir = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
-            } else {
-                // Documentsディレクトリを作成して保存する
-                outputDir = File(context.getExternalFilesDir(null), "Documents")
-            }
+        var outputFile: File? = getFileName(context, outputDir)
 
-            if (outputDir == null) return null
+        if (outputFile == null || writeToFile(outputFile, memo)) return null
 
-            var hasDirectory: Boolean = true
-            if (!outputDir.exists() || !outputDir.isDirectory) hasDirectory = outputDir.mkdir()
-            if (!hasDirectory) return null
+        // タイトルは本文から決定する
+        var title: String = if (memo.length > 10) memo.substring(0, 10) else memo
 
-            var outputFile: File = saveAsFile(context, outputDir, memo)
-            if (outputFile == null) return null
+        // DBに登録するその他の情報
+        var values: ContentValues = ContentValues()
+        values.put(MemoDBHelper.TITLE, title)
+        values.put(MemoDBHelper.DATA, outputFile.absolutePath)
+        values.put(MemoDBHelper.DATE_ADDED, System.currentTimeMillis())
 
-            // タイトルは本文から決定する
-            var title: String = if (memo.length > 10) memo.substring(0, 10) else memo
+        // ContentProviderに保存する
+        return context.contentResolver.insert(MemoProvider.CONTENT_URI, values)
+    }
 
-            // DBに保存するためContentValuesに詰める
-            var values: ContentValues = ContentValues()
-            values.put(MemoDBHelper.TITLE, title)
-            values.put(MemoDBHelper.DATA, outputFile.absolutePath)
-            values.put(MemoDBHelper.DATE_ADDED, System.currentTimeMillis())
+    // 既存のメモの更新
+    fun update(context: Context, uri: Uri, memo: String): Int {
+        // 引数のURIを元にDB検索を行う
+        var id: String = uri.lastPathSegment
+        var cursor: Cursor? = context.contentResolver.query(uri,
+            arrayOf(MemoDBHelper.DATA), MemoDBHelper._ID + " = ?", arrayOf(id), null)
 
-            // ContentProviderに保存する
-            return context.contentResolver.insert(MemoProvider.CONTENT_URI, values)
+        cursor ?: return 0
+
+        var filePath: String? = null
+
+        while (cursor.moveToNext()) {
+            filePath = cursor.getString(cursor.getColumnIndex(MemoDBHelper.DATA))
         }
 
-        private fun saveAsFile(context: Context, outputDir: File, memo: String): File? {
-            var fileNamePrefix: String = SettingPrefUtil.getFileNamePrefix(context)
+        cursor.close()
 
-            // 現在日時
-            var now: Calendar = Calendar.getInstance()
+        // スマートキャスト
+        if (filePath == null || filePath.isEmpty()) return 0
 
-            // ファイル名の決定
-            var fileName: String = String.format(MEMO_FILE_FORMAT, fileNamePrefix, now)
+        var outputFile: File = File(filePath)
 
-            var outputFile: File = File(outputDir, fileName)
-            var writer: FileWriter? = null
+        if (writeToFile(outputFile, memo)) return 0
 
-            try {
-                writer = FileWriter(outputFile)
-                writer.write(memo)
-                writer.flush()
-            } catch (e: IOException) {
-                e.printStackTrace()
-                return null
-            } finally {
-                if (writer != null) {
-                    try {
-                        writer.close()
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
+        return 1
+    }
+
+    // メモの読み込み
+    fun findMemoByUri(context: Context, uri: Uri): String {
+        var reader: BufferedReader? = null
+        var builder: StringBuilder = StringBuilder()
+
+        try {
+            // ??
+            var inputStream: InputStream? = context.applicationContext.openInputStream(uri)
+            inputStream?.let {
+                reader = BufferedReader(InputStreamReader(inputStream))
+
+                var line: String
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line)
+                    builder.append("\n")
                 }
             }
 
-            return outputFile
+        } catch (fnfe: FileNotFoundException) {
+            // ファイルが削除されるなどして見つからなかった場合
+            fnfe.printStackTrace()
+            return context.getString(R.string.error_memo_file_not_found)
+        } catch (ioe: IOException) {
+            // ファイルの読み込みに失敗した場合
+            ioe.printStackTrace()
+            return context.getString(R.string.error_memo_file_load_failed)
+        } finally {
+            reader?.let {
+                try {
+                    reader.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
         }
+
+        return builder.toString()
+    }
+
+    fun query(context: Context): Cursor {
+        return context.contentResolver.query(
+            MemoProvider.CONTENT_URI,
+            null, null, null, MemoDBHelper.DATE_MODIFIED + " DESC"
+        )
+    }
+
+    private fun getOutputDir(context: Context): File? {
+        var outputDir: File?
+
+        if (Build.VERSION.SDK_INT >= 19) {
+            // ドキュメントファイル用共有ディレクトリ
+            // このディレクトリを得るための定数が19以上にしか存在しない
+            outputDir = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+        } else {
+            // Documentsディレクトリを作成して保存する
+            outputDir = File(context.getExternalFilesDir(null), "Documents")
+        }
+
+        // 外部スレージがマウントされていない等
+        outputDir ?: return null
+
+        var isExist: Boolean = true
+        if (!outputDir.exists() || outputDir.mkdirs()) isExist = outputDir.mkdirs()
+
+        return if (isExist) outputDir else null
+    }
+
+    // 出力ファイルの取得
+    private fun getFileName(context: Context, outputDir: File): File {
+        var fileNamePrefix: String = SettingPrefUtil.getFileNamePrefix(context)
+
+        // 現在日時
+        var now: Calendar = Calendar.getInstance()
+
+        // ファイル名の決定
+        var fileName: String = String.format(MemoRepository.MEMO_FILE_FORMAT, fileNamePrefix, now)
+
+        return File(outputDir, fileName)
+    }
+
+    // ファイルにメモを書き込む
+    private fun writeToFile(outputFile: File, memo: String): Boolean {
+        var writer: FileWriter? = null
+
+        try {
+            writer = FileWriter(outputFile)
+            writer.write(memo)
+            writer.flush()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return false
+        } finally {
+            writer?.let {
+                try {
+                    writer.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+        return true
     }
 }
